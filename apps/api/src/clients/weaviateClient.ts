01c2fa28e3bdccdf46e4e277c273ba7ee410133f
maksymilianpadalak <maksymilian.padalak@gmail.com>
2025-09-19 16:29:02 +0200

    Update weaviate endpoint

--- apps/api/src/clients/weaviateClient.ts
+++ apps/api/src/clients/weaviateClient.ts
@@ -2,7 +2,7 @@ import weaviate from 'weaviate-ts-client';
 
 const client = weaviate.client({
   scheme: 'https',
-  host: 'sxxexhf1t0gged4nutoktw.c0.europe-west3.gcp.weaviate.cloud',
+  host: 'xyxahx3qiojl1twyhpiuq.c0.europe-west3.gcp.weaviate.cloud',
   apiKey: new weaviate.ApiKey(process.env?.WEAVIATE_API_KEY || ''),
   headers: {
     'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY || '',
